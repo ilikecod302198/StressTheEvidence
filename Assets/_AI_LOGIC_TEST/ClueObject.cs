e25e53eb2dbd138e1666ebc353ce3b9fa93cc7c5
jmathew2354 <johaan.idiculla@gmail.com>
2026-03-29 00:10:41 +1100

    Adding attributes to clue objects

---
+++ Assets/_AI_LOGIC_TEST/ClueObject.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class ClueObject : MonoBehaviour
+{
+    public string clueName = "Crowbar";
+    [TextArea] public string clueDescription = "A heavy crowbar with dried blood on the end. Someone left this behind.";
+    public bool isRealClue = true;
+
+    public GameObject inspectUI;
+    public TMP_Text inspectText;
+    public GameObject pressEPrompt;
+
+    private bool playerNearby = false;
+    private bool collected = false;
+
+    void Update()
+    {
+        if (playerNearby && !collected)
+        {
+            pressEPrompt.SetActive(true);
+
+            if (Keyboard.current.eKey.wasPressedThisFrame)
+            {
+                collected = true;
+                ClueManager.Instance.AddClue(clueName, isRealClue);
+                inspectText.text = clueDescription;
+                pressEPrompt.SetActive(false);
+                inspectUI.SetActive(true);
+                Invoke("HideUI", 3f);
+            }
+        }
+    }
+
+    void HideUI() { inspectUI.SetActive(false); }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) playerNearby = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNearby = false;
+            pressEPrompt.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Something entered trigger: " + other.name);
+        if (other.CompareTag("Player")) playerNearby = true;
+    }
+}
