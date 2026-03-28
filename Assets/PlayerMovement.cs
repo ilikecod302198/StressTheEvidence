64d700a8838d3035e0a0c5a522f87b3f428e2606
ilikecod302198 <markonikodinovski@gmail.com>
2026-03-28 16:31:11 +1100

    Walking (no animation)

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 3.9f;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        movement = movement.normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = movement * moveSpeed;
+    }
+}
