1885cd6923ef1cd41cf797fcc3029fed4f565492
jmathew2354 <johaan.idiculla@gmail.com>
2026-03-29 13:43:13 +1100

    Developed Process to load to interrogation scene

---
+++ Assets/_AI_LOGIC_TEST/ExitTrigger.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class ExitTrigger : MonoBehaviour
+{
+    public TMP_Text exitPrompt;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+            exitPrompt.enabled = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+            exitPrompt.enabled = false;
+    }
+
+    void Update()
+    {
+        if (exitPrompt.enabled && Keyboard.current.spaceKey.wasPressedThisFrame)
+            SceneManager.LoadScene("InterrogationScene");
+    }
+}
