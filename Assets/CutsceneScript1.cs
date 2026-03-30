05cff1b8de17bc2ea267375606e88cf525a6c2d1
jmathew2354 <johaan.idiculla@gmail.com>
2026-03-30 20:14:36 +1100

    Made first prototype

--- Assets/CutsceneScript1.cs
+++ Assets/CutsceneScript1.cs
@@ -1,18 +1,34 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class CutsceneManager : MonoBehaviour
 {
+    public UnityEngine.UI.Image displayImage;
+    public Sprite[] images;
+    public float timePerImage = 3f;
     public string nextScene = "JewleryStore";
-    public float displayTime = 5f;
+
+    private int currentIndex = 0;
 
     void Start()
     {
-        Invoke("LoadNextScene", displayTime);
+        if (images.Length > 0)
+            displayImage.sprite = images[0];
+        Invoke("NextImage", timePerImage);
     }
 
-    void LoadNextScene()
+    void NextImage()
     {
-        SceneManager.LoadScene(nextScene);
+        currentIndex++;
+        if (currentIndex < images.Length)
+        {
+            displayImage.sprite = images[currentIndex];
+            Invoke("NextImage", timePerImage);
+        }
+        else
+        {
+            SceneManager.LoadScene(nextScene);
+        }
     }
 }
