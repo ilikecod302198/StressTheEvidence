407585871c7ba6980be3d945cb4b8ebb5e2a02b7
jmathew2354 <johaan.idiculla@gmail.com>
2026-03-30 20:42:21 +1100

    Audio Finalised

--- Assets/CutsceneScript1.cs
+++ Assets/CutsceneScript1.cs
@@ -6,29 +6,42 @@ public class CutsceneManager : MonoBehaviour
 {
     public UnityEngine.UI.Image displayImage;
     public Sprite[] images;
+    public AudioClip[] audioClips;
     public float timePerImage = 3f;
-    public string nextScene = "JewleryStore";
+    public string nextScene = "MainMenu";
 
     private int currentIndex = 0;
+    private AudioSource audioSource;
 
     void Start()
-    {
-        if (images.Length > 0)
-            displayImage.sprite = images[0];
-        Invoke("NextImage", timePerImage);
-    }
-
-    void NextImage()
-    {
-        currentIndex++;
-        if (currentIndex < images.Length)
         {
-            displayImage.sprite = images[currentIndex];
+            audioSource = GetComponent<AudioSource>();
+            if (images.Length > 0)
+            {
+                displayImage.sprite = images[0];
+                PlayAudio(0);
+            }
             Invoke("NextImage", timePerImage);
         }
-        else
+
+        void NextImage()
         {
-            SceneManager.LoadScene(nextScene);
+            currentIndex++;
+            if (currentIndex < images.Length)
+            {
+                displayImage.sprite = images[currentIndex];
+                PlayAudio(currentIndex);
+                Invoke("NextImage", timePerImage);
+            }
+            else
+            {
+                SceneManager.LoadScene(nextScene);
+            }
         }
-    }
+
+        void PlayAudio(int index)
+        {
+            if (audioSource != null && audioClips.Length > index && audioClips[index] != null)
+                audioSource.PlayOneShot(audioClips[index]);
+        }
 }
